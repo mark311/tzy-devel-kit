a3d7830c7d295e43bab316be8ea604bee710148f
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2014-08-13 21:01:35 +0800

    Initial

---
+++ bash_profile
@@ -0,0 +1,4 @@
+#!/bin/sh
+alias em=emacs
+export PS1='\n\e[1;37m[\e[m\e[1;32m\u\e[m\e[1;33m@\e[m\e[1;35m\h\e[m \e[4m`pwd`\e[m\e[1;37m]\e[m\e[1;36m\e[m\n\$ '
+export EDITOR=emacs
---
+++ emacs.d/init.el
@@ -0,0 +1,51 @@
+;; Common
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "/usr/local/share/gtags")
+
+(ido-mode)
+;; (desktop-save-mode 1)
+(setq completion-ignore-case nil)
+
+(global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down 1)))
+(global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up 1)))
+
+(defun move-end-of-line-and-newline-and-indent()
+  "At anywhere in a line, goto end and indent-return"
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent)
+)
+(global-set-key (kbd "C-M-j") 'move-end-of-line-and-newline-and-indent)
+
+(defun move-beginning-of-line-and-open-line-and-indent()
+  "At anywhere in a line, insert an empty line before it and indent"
+  (interactive)
+  (move-beginning-of-line nil)
+  (open-line 1)
+  (indent-for-tab-command nil)
+)
+(global-set-key (kbd "C-o") 'move-beginning-of-line-and-open-line-and-indent)
+
+;; Gtags/Global
+(autoload 'gtags-mode "gtags" "" t)
+(add-hook 'c-mode-common-hook
+   '(lambda ()
+      (gtags-mode 1)
+))
+(add-hook 'gtags-select-mode-hook
+  '(lambda ()
+     (setq hl-line-face 'underline)
+     (hl-line-mode 1)
+))
+(setq gtags-suggested-key-mapping t)
+(setq gtags-auto-update t)
+
+;; Coding Style
+(setq c-default-style "linux"
+          c-basic-offset 4)
+(setq-default indent-tabs-mode nil)
+
+;; Highlight
+(require 'highlight-symbol)
+(global-set-key (kbd "<f8>") 'highlight-symbol-at-point)
+
