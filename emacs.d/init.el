1771db44665e29282f3f408a5abb8100dc19242c
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2014-09-01 16:18:03 +0800

    [emacs] mouse wheel accelarate off, step 3 lines.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -78,6 +78,16 @@
 
 ;; Custom Set Variables
 (custom-set-variables
- ;; Make ediff-* split windows horizontally (left and right)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(ediff-split-window-function (quote split-window-horizontally))
-)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (3 ((shift) . 1) ((control))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
