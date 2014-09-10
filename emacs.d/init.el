37c2c9eb16a82c79876d0dde22f3c25a20862ea0
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2014-09-10 17:27:55 +0800

    [emacs] Adjusted diff-mode color face

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -105,6 +105,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:inherit diff-changed :foreground "green"))))
+ '(diff-refine-added ((t (:inherit diff-refine-change))))
+ '(diff-refine-change ((t (:weight bold))))
+ '(diff-refine-removed ((t (:inherit diff-refine-change))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "red"))))
  '(font-lock-builtin-face ((t (:foreground "white"))))
  '(helm-buffer-directory ((t (:background "black" :foreground "brightblue"))))
  '(helm-ff-directory ((t (:background "black" :foreground "brightblue"))))
