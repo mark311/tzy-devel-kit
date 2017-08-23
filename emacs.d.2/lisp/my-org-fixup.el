8924cdb59989e455a4a8cfdecac1601aba048e0e
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2017-08-23 22:09:38 +0800

    [emacs] Join consecutive Chinese lines into a single long line without unwanted space when exporting org-mode to html.

---
+++ emacs.d.2/lisp/my-org-fixup.el
@@ -0,0 +1,17 @@
+;; See section "中文中，转成HTML时，换行符成空格符号" in
+;; http://wiki.dreamrunner.org/public_html/Emacs/org-mode.html
+;;
+(defadvice org-html-paragraph (before org-html-paragraph-advice
+                                      (paragraph contents info) activate)
+  "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+  (let* ((origin-contents (ad-get-arg 1))
+         (fix-regexp "[[:multibyte:]]")
+         (fixed-contents
+          (replace-regexp-in-string
+           (concat
+            "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+
+    (ad-set-arg 1 fixed-contents)))
+
+(provide 'my-org-fixup)
