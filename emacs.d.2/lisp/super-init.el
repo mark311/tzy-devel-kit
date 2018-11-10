0ea071a38ecdee2b01f0737cbf4156d1574a7da4
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2018-11-11 00:41:23 +0800

    [emacs][melpa] disable melpa (keep melpa-stable)

--- emacs.d.2/lisp/super-init.el
+++ emacs.d.2/lisp/super-init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
