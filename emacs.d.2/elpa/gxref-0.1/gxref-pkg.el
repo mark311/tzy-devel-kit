fcead34bfaf0951c1abd4a229e6d18fda7d68f01
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2018-11-10 22:57:09 +0800

    [emacs] check in elpa packages into git repository

---
+++ emacs.d.2/elpa/gxref-0.1/gxref-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gxref" "0.1" "xref backend using GNU Global." '((emacs "25")) :commit "15723a9d910d7dd9ea18cab0336332cf988aeceb" :url "https://github.com/dedi/gxref" :keywords '("xref" "global" "tools"))
