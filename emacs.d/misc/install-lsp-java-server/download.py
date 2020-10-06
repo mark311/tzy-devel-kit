bb0d9206db71cb8487fa9358367fa2f40f656fc7
zhiyang.tzy <zhiyang.tzy@alibaba-inc.com>
2020-10-06 09:21:01 +0800

    update

--- emacs.d/misc/install-lsp-java-server/download.py
+++ emacs.d/misc/install-lsp-java-server/download.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+# use socks5 proxy:
+#
+# $ env https_proxy=socks5://127.0.0.1:1089 python download.py
+
 import sys,os
 import subprocess
 
 urls_to_download = [
-    ("https", "download.eclipse.org", "jdtls/milestones/0.62.0/jdt-language-server-0.62.0-202009291815.tar.gz"),
-    ("https", "github.com"          , "emacs-lsp/lsp-java/releases/download/2.2/Pivotal.vscode-spring-boot-1.6.0.vsix"),
-    ("https", "github.com"          , "emacs-lsp/lsp-java/releases/download/2.2/dgileadi.java-decompiler-0.0.2.vsix"),
-    ("https", "github.com"          , "emacs-lsp/lsp-java/releases/download/2.2/vscjava.vscode-java-dependency-0.5.1.vsix"),
-    ("https", "github.com"          , "emacs-lsp/lsp-java/releases/download/3.1/vscjava.vscode-java-test-0.24.1.vsix"),
+    ("https", "raw.githubusercontent.com", "emacs-lsp/lsp-java/master/install/pom.xml"),
+    ("https", "download.eclipse.org"     , "jdtls/milestones/0.62.0/jdt-language-server-0.62.0-202009291815.tar.gz"),
+    ("https", "github.com"               , "emacs-lsp/lsp-java/releases/download/2.2/Pivotal.vscode-spring-boot-1.6.0.vsix"),
+    ("https", "github.com"               , "emacs-lsp/lsp-java/releases/download/2.2/dgileadi.java-decompiler-0.0.2.vsix"),
+    ("https", "github.com"               , "emacs-lsp/lsp-java/releases/download/2.2/vscjava.vscode-java-dependency-0.5.1.vsix"),
+    ("https", "github.com"               , "emacs-lsp/lsp-java/releases/download/3.1/vscjava.vscode-java-test-0.24.1.vsix"),
 ]
 
 for url in urls_to_download:
@@ -19,4 +24,4 @@ for url in urls_to_download:
        os.makedirs(dest_file_parent_dir)
 
     print "downloading", url
-    subprocess.check_call(['curl', '%s://%s/%s' % url, '-O', dest_file])
+    subprocess.check_call(['curl', '-L', '%s://%s/%s' % url, '-o', dest_file])
